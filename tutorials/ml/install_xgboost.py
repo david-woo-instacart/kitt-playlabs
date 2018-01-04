7f93b62bd49d4cf9f34b10054c302094fe24293a
David Woo <david.woo@instacart.com>
2018-01-03 20:16:35 -0800

    instructions to install xgboost

---
+++ tutorials/ml/install_xgboost.py
@@ -0,0 +1,31 @@
+# Guide to installing xgboost
+
+# Reference:
+#https://isaacchanghau.github.io/2017/06/20/Install-XGBoost-on-Mac-OS-X/
+
+#Uninstall all versions of gcc, so that we know which version is being referenced
+brew uninstall gcc
+#Then reinstall
+brew install gcc --without-multilib
+
+#Clone xgboost repo
+git clone --recursive https://github.com/dmlc/xgboost
+#Build repo
+cd xgboost
+$ vi make/config.mk
+#Open config File and uncomment and change the following lines
+export CC = gcc-5
+export CXX = g++-5
+# Then build repo
+cp make/config.mk .
+$ sudo make -j4 # sudo is important, prevent permission denied issue
+
+#Once the above is done, following python package installing here:
+#http://xgboost.readthedocs.io/en/latest/build.html#python-package-installation
+cd python-package; sudo python setup.py install
+
+#add python page to python path
+export PYTHONPATH=~/xgboost/python-package
+
+#navigate to python package and install
+cd python-package; python setup.py develop --user
